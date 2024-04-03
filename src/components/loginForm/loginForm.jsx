0279582366fee5346ab2d77fa528e83e7e80ab4e
Anjesh Bhattarai <bhattaraianjesh123@gmail.com>
2024-04-03 15:50:20 +1030

    applying Comments

--- src/components/loginForm/loginForm.jsx
+++ src/components/loginForm/loginForm.jsx
@@ -6,14 +6,27 @@ import { useFormState } from "react-dom";
 import Link from "next/link";
 
 const LoginForm = () => {
-  const [state, formAction] = useFormState(login, undefined);
+// Destructure the return values of the useFormState hook into two variables: state and formAction
+// 'state' represents the current state of the form, which includes values like error messages, success status, etc.
+// 'formAction' represents the function to be called when the form is submitted, typically used as the form's action attribute
+
+const [state, formAction] = useFormState(login, undefined);
+// Here, 'login' is a function defined in action.js responsible for handling the login action.
+// The second argument, 'undefined', is the default value of the form state.
+// If the 'login' function returns 'undefined', then the default value of the form state will be 'undefined'.
+// If 'login' returns some other value, then that value will be the default state of the form.
+
 
   return (
     <form className={styles.form} action={formAction}>
+      {/* Input fields for username and password */}
       <input type="text" placeholder="username" name="username" />
       <input type="password" placeholder="password" name="password" />
+      {/* Login button */}
       <button>Login</button>
+      {/* Display error message from state */}
       {state?.error}
+      {/* Link to registration page */}
       <Link href="/register">
         {"Don't have an account?"} <b>Register</b>
       </Link>
--- src/components/registerForm/registerForm.jsx
+++ src/components/registerForm/registerForm.jsx
@@ -9,18 +9,24 @@ import { useRouter } from "next/navigation";
 import Link from "next/link";
 
 const RegisterForm = () => {
-  const [state, formAction] = useFormState(register, undefined); //register is a function defined in action.js, and the undefined is the default value of the state,
-  // if register retuns undefined, then the default value of the state will be undefined but if
-  //if register retuns some, then the default value of the state will be that value
+  const [state, formAction] = useFormState(register, undefined);
+  // register is a function defined in action.js, and the undefined is the default value of the state,
+  // if register returns undefined, then the default value of the state will be undefined,
+  // but if register returns some value, then the default value of the state will be that value
 
+  // Get router object using useRouter hook
   const router = useRouter();
 
+  // Use useEffect hook to perform side effects based on state and router changes
   useEffect(() => {
+    // If state.success is true, redirect to "/login" page
     state?.success && router.push("/login");
-  }, [state?.success, router]);
+  }, [state?.success, router]); // Dependencies: state.success and router
 
+  // Return JSX for the RegisterForm component
   return (
     <form className={styles.form} action={formAction}>
+      {/* Input fields for username, email, password, and password repeat */}
       <input type="text" placeholder="username" name="username" />
       <input type="email" placeholder="email" name="email" />
       <input type="password" placeholder="password" name="password" />
@@ -29,8 +35,11 @@ const RegisterForm = () => {
         placeholder="password again"
         name="passwordRepeat"
       />
+      {/* Register button */}
       <button>Register</button>
+      {/* Display error message from state */}
       {state?.error}
+      {/* Link to login page */}
       <Link href="/login">
         Have an account? <b>Login</b>
       </Link>
